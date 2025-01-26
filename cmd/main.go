5fd7a55444169134b8210fa901498007bc0efe66
stkn <matchstickn@gmail.com>
2025-01-26 00:51:23 -0500

    added queries and schemas + sqlc gen

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"database/sql"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
+)
 
 func main() {
-	fmt.Println("hello")
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	pq, err := sql.Open("postgres", "user=postgres password="+os.Getenv("password")+" dbname=FlintCRUD sslmode=disable")
+	if err != nil {
+		log.Fatalf("failed opening connection to posgresql: %v", err)
+	}
+	defer pq.Close()
 }
---
+++ query.sql
@@ -0,0 +1,23 @@
+-- name: GetTrick :one
+SELECT * FROM tricks
+WHERE id = $1 LIMIT 1;
+
+-- name: GetAllTricks :many
+SELECT * FROM tricks 
+ORDER BY name;
+
+-- name: CreateTrick :one
+INSERT INTO tricks 
+(name, style, power) 
+VALUES ($1, $2, $3) 
+RETURNING *;
+
+-- name: UpdateTrick :exec
+UPDATE tricks
+SET name = $2, style = $3, power = $4
+WHERE id = $1
+RETURNING *;
+
+-- name: DeleteTrick :exec
+DELETE FROM tricks
+WHERE id = $1;
